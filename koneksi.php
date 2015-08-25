fb1b70dedd645a3ae8355792d332a77ddb5e0a25
Putra Rahmadi <rapz.putra@gmail.com>
2015-08-25 10:08:05 +0700

    koneksi_db_privacy

--- koneksi.php
+++ koneksi.php
@@ -13,20 +13,4 @@
 	else{
 		die("Server MySQL tidak terhubung");
 	}
-/*
-$host = "localhost";
-$user = "root";
-$pass = "yogyakarta1";
-$dbnm = "bank";
-$rp = mysql_connect ($host, $user, $pass);
-if($rp){
-	$buka = mysql_select_db ($dbnm);
-	if(!$buka){
-		die("Database tidak dapat dibuka");
-	}
-}
-else{
-	die("Server MySQL tidak terhubung");
-}
-*/
 ?>
